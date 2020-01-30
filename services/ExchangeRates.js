9893915747b8f444b7515852cdec6ebb885cd712
Edu Moyano <edu@syncrtc.com>
2020-01-30 13:18:01 +0100

    Added exchange rates service

---
+++ services/ExchangeRates.js
@@ -0,0 +1,39 @@
+const fetch = require('node-fetch');
+
+/* Service that connects to exchangeratesapi.io to retrieve exchange rates data */
+class ExchangeRatesService {
+  constructor() {
+    this.apiURL = 'https://api.exchangeratesapi.io';
+  }
+
+  /* Gets the current exchange rate between two currencies
+   * @param {string} sellCurrency - Currency code to sell (in ISO 4217 format)
+   * @param {string} buyCurrency - Currency code to buy (in ISO 4217 format)
+   * @return {float} - The exchange rate between those two currencies
+   */
+  async getExchangeRate(sellCurrency, buyCurrency) {
+    const endpoint = 'latest';
+    const response = await fetch(`${this.apiURL}/${endpoint}?base=${sellCurrency}&symbols=${buyCurrency}`);
+    const json = await response.json();
+    if(!json.error) {
+      return json.rates[buyCurrency].toFixed(4);
+    }
+    return null;
+  }
+
+  /* Gets a list of available currencies to be converted
+   * @return {array} - Array containing a list of available currencies (in ISO 4217 format)
+   */
+  async getAvailableCurrencies() {
+    const endpoint = 'latest';
+    const response = await fetch(`${this.apiURL}/${endpoint}`);
+    const json = await response.json();
+    if(!json.error) {
+      return Object.keys(json.rates).concat(json.base);
+    }
+    return [];
+  }
+}
+
+
+module.exports = ExchangeRatesService;
---
+++ tests/unit/ExchangeRatesService.test.js
@@ -0,0 +1,46 @@
+const fetch = require('node-fetch');
+const ExchangeRatesService = require('../../services/ExchangeRates');
+const currencyCodesResponse = require('../testData/currencyCodesResponse.json');
+
+jest.mock('node-fetch', () => jest.fn());
+const exchangeRates = new ExchangeRatesService();
+
+
+describe('Get an exchange rate', () => {
+  it('Should get an exchange rate', async () => {
+    const returnBody = { rates: { EUR: 0.9086778737 }, base: 'USD', date: '2020-01-28' };
+    const response = Promise.resolve({
+      ok: true,
+      status: 'ok',
+      json: () => returnBody,
+    });
+    fetch.mockImplementation(() => response);
+    const result = await exchangeRates.getExchangeRate('USD', 'EUR');
+    expect(result).toBe(returnBody.rates.EUR);
+  });
+
+  it('Should return null if currency codes are not valid', async () => {
+    const returnBody = { error: 'Symbols \'TTT\' are invalid for date 2020-01-28.' };
+    const response = Promise.resolve({
+      ok: true,
+      status: 'ok',
+      json: () => returnBody,
+    });
+    fetch.mockImplementation(() => response);
+    const result = await exchangeRates.getExchangeRate('EUR', 'TTT');
+    expect(result).toBeNull();
+  });
+});
+
+describe('Get available currencies', () => {
+  it('Should get all available currencies at the moment', async () => {
+    const response = Promise.resolve({
+      ok: true,
+      status: 'ok',
+      json: () => currencyCodesResponse,
+    });
+    fetch.mockImplementation(() => response);
+    const result = await exchangeRates.getAvailableCurrencies();
+    expect(result).toEqual(Object.keys(currencyCodesResponse.rates).concat(currencyCodesResponse.base));
+  });
+});
