c1ee325836d44b27d94833eea4760219035c1037
Edu Moyano <edu@syncrtc.com>
2020-01-30 22:58:59 +0100

    Mock api calls in tests

--- tests/exchange.test.js
+++ tests/exchange.test.js
@@ -1,5 +1,9 @@
+const fetch = require('node-fetch');
 const request = require('supertest');
 const app = require('../app');
+const currencyCodesResponse = require('./testData/currencyCodesResponse.json');
+
+jest.mock('node-fetch', () => jest.fn());
 
 const baseEndpoint = '/api/exchange';
 
@@ -16,6 +20,12 @@ jest.mock('pg', () => {
 describe('Get available currencies', () => {
   const path = 'currencies';
   it('Returns code 200 if able to get available currencies', async () => {
+    const response = Promise.resolve({
+      ok: true,
+      status: 'ok',
+      json: () => currencyCodesResponse,
+    });
+    fetch.mockImplementation(() => response);
     const res = await request(app).get(`${baseEndpoint}/${path}`).send();
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe('ok');
@@ -26,8 +36,15 @@ describe('Get available currencies', () => {
 
 describe('Get current exchange rate', () => {
   const path = 'rate';
-  it('Returns code 200 if able to get available currencies', async () => {
-    const res = await request(app).get(`${baseEndpoint}/${path}?sellCurrency=EUR&buyCurrency=USD`).send();
+  it('Returns code 200 if able to get the exchange rate', async () => {
+    const returnBody = { rates: { EUR: 0.9086778737 }, base: 'USD', date: '2020-01-28' };
+    const response = Promise.resolve({
+      ok: true,
+      status: 'ok',
+      json: () => returnBody,
+    });
+    fetch.mockImplementationOnce(() => response);
+    const res = await request(app).get(`${baseEndpoint}/${path}?sellCurrency=USD&buyCurrency=EUR`).send();
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe('ok');
     expect(res.body.result).not.toBeNaN();
@@ -35,6 +52,13 @@ describe('Get current exchange rate', () => {
   });
 
   it('Returns code 200 with error if currencies are not valid', async () => {
+    const returnBody = { error: 'Symbols \'TTT\' are invalid for date 2020-01-28.' };
+    const response = Promise.resolve({
+      ok: true,
+      status: 'ok',
+      json: () => returnBody,
+    });
+    fetch.mockImplementation(() => response);
     const res = await request(app).get(`${baseEndpoint}/${path}?sellCurrency=EUR&buyCurrency=TTT`).send();
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe('error');
